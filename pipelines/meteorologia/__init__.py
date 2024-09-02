cc2dca972010d7832a4fc51739e644fda788873e
patriciacatandi <patriciabcatandi@gmail.com>
2024-09-02 15:07:33 -0300

    adding templates and flows.py

---
+++ pipelines/meteorologia/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from pipelines.meteorologia.meteorologia_inmet.flows import *  # noqa
+from pipelines.meteorologia.meteorologia_redemet.flows import *  # noqa
+from pipelines.meteorologia.precipitacao_alertario.flows import *  # noqa
+from pipelines.meteorologia.precipitacao_cemaden.flows import *  # noqa
+from pipelines.meteorologia.precipitacao_inea.flows import *  # noqa
+from pipelines.meteorologia.precipitacao_websirene.flows import *  # noqa
+from pipelines.meteorologia.satelite.flows import *  # noqa
