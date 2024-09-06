3ec0c7463c4c5504d0d249739ed34759f0f67b09
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2024-09-06 16:32:42 -0300

    fix: flow imports

--- pipelines/__init__.py
+++ pipelines/__init__.py
@@ -1,13 +1,6 @@
 # -*- coding: utf-8 -*-
-
 """
-Prefect flows for cor project
+Imports all flows for every project so we can register all of them.
 """
-from pipelines.bot_semaforo.flows import *
-from pipelines.meteorologia.meteorologia_inmet.flows import *
-from pipelines.meteorologia.meteorologia_redemet.flows import *
-from pipelines.meteorologia.precipitacao_alertario.flows import *
-from pipelines.meteorologia.precipitacao_cemaden.flows import *
-from pipelines.meteorologia.precipitacao_inea.flows import *
-from pipelines.meteorologia.satelite.flows import *
-from pipelines.meteorologia.precipitacao_websirene.flows import *
+from pipelines.meteorologia import *
+from pipelines.templates import *
