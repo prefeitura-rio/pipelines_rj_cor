1a045355d45524fd8e3d72e4e20c51fc3acad482
patriciacatandi <patriciabcatandi@gmail.com>
2024-09-18 17:25:58 -0300

    removing example flow

--- pipelines/flows.py
+++ pipelines/flows.py
@@ -2,6 +2,5 @@
 """
 Imports all flows for every project so we can register all of them.
 """
-from pipelines.exemplo import *  # noqa
 from pipelines.meteorologia import *  # noqa
 from pipelines.templates import *  # noqa
